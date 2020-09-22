3ea9f7cf57ebb3aedc95e1d9e896e8f9c62515cf
Damian Czarnota <godevdamian@gmail.com>
2020-09-22 14:41:46 +0200

    refactor of components expression

--- src/components/Courses/index.js
+++ src/components/Courses/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -9,71 +10,76 @@ const Wrapper = styled.div`
     width: 90%;
   } ;
 `;
+
 const CoursesHeader = styled.div`
   display: flex;
   justify-content: space-between;
   width: 90%;
   margin: 0 auto;
+`;
 
-  button {
-    color: #2575e6;
-    background-color: white;
-    border: 1px solid #2575e6;
-    border-radius: 10px;
-    outline: none;
-    padding: 10px 3px;
-    font-size: ${({ theme }) => theme.font.size.xxs};
-    cursor: pointer;
-  }
-  h2 {
-    font-size: ${({ theme }) => theme.font.size.sm};
-    position: relative;
-    &:after {
-      content: "";
-      display: block;
-      position: absolute;
-      left: 0;
-      bottom: -20px;
-      width: 50%;
-      height: 3px;
-      background-color: #006aff;
-    }
+const CoursesTitle = styled.h2`
+  font-size: ${({ theme }) => theme.font.size.sm};
+  position: relative;
+  &:after {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 0;
+    bottom: -20px;
+    width: 50%;
+    height: 3px;
+    background-color: #006aff;
   }
   ${({ theme }) => theme.mq.md} {
-    h2 {
-      font-size: ${({ theme }) => theme.font.size.lg};
-    }
-    button {
-      font-size: ${({ theme }) => theme.font.size.xs};
-      padding: 15px 5px;
-    }
+    font-size: ${({ theme }) => theme.font.size.lg};
   }
 `;
+const CoursesBtn = styled.button`
+  color: #2575e6;
+  background-color: white;
+  border: 1px solid #2575e6;
+  border-radius: 10px;
+  outline: none;
+  padding: 10px 3px;
+  font-size: ${({ theme }) => theme.font.size.xxs};
+  cursor: pointer;
+
+  ${({ theme }) => theme.mq.md} {
+    font-size: ${({ theme }) => theme.font.size.xs};
+    padding: 15px 5px;
+  }
+`;
+
 const CoursesWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
   align-items: center;
 `;
+
 const Card = styled.div`
   display: flex;
   width: 33%;
   border-radius: 20px;
   flex-direction: column;
 `;
+
 const CardImage = styled.div`
   height: 60%;
 `;
+
 const CardContent = styled.div`
   height: 40%;
   padding: 10px;
 `;
-function Courses() {
+
+const Courses = () => {
   return (
     <Wrapper>
       <CoursesHeader>
-        <h2>Our Popular Courses</h2>
-        <button>ALL COURSES</button>
+        <CoursesTitle>Our Popular Courses</CoursesTitle>
+        <CoursesBtn>ALL COURSES</CoursesBtn>
       </CoursesHeader>
       <CoursesWrapper>
         <Card>
@@ -82,6 +88,6 @@ function Courses() {
       </CoursesWrapper>
     </Wrapper>
   );
-}
+};
 
 export default Courses;
--- src/components/Statistics/index.js
+++ src/components/Statistics/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import image1 from "../../assets/images/1.png";
 import image2 from "../../assets/images/2.png";
 import image3 from "../../assets/images/3.png";
+
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -10,33 +11,34 @@ const Wrapper = styled.div`
   margin: 0 auto;
   align-items: center;
   justify-content: center;
-  p {
-    flex: 1;
-    img {
-      width: 70%;
-    }
-  }
   ${({ theme }) => theme.mq.md} {
     flex-direction: row;
     width: 85%;
-    p {
-      width: 100%;
-    }
+  }
+`;
+const ImageParagraph = styled.p`
+  flex: 1;
+  img {
+    width: 70%;
+  }
+  ${({ theme }) => theme.mq.md} {
+    width: 100%;
   }
 `;
 
 const images = [image1, image2, image3];
-function Statisctics() {
+
+const Statisctics = () => {
   return (
     <Wrapper>
       {images.map((image) => {
         return (
-          <p>
+          <ImageParagraph>
             <img src={image} alt='image' />
-          </p>
+          </ImageParagraph>
         );
       })}
     </Wrapper>
   );
-}
+};
 export default Statisctics;
